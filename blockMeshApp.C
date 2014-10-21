a3a7302a0a34e45ef7788cf3d1feae519e429eae
Bruno Santos <bruno.santos@bluecape.com.pt>
2014-10-21 10:17:13 +0100

    Adapted code to be compilable with blueCFD-Core 2.3.

--- blockMeshApp.C
+++ blockMeshApp.C
@@ -53,7 +53,7 @@ Usage
 
 \*---------------------------------------------------------------------------*/
 
-#include "Time.H"
+#include "Time.T.H"
 #include "IOdictionary.H"
 #include "IOPtrList.H"
 
@@ -66,7 +66,7 @@ Usage
 #include "OSspecific.H"
 #include "OFstream.H"
 
-#include "Pair.H"
+#include "Pair.T.H"
 #include "slidingInterface.H"
 
 using namespace Foam;
--- lib/blockMesh/blockMeshTopology.C
+++ lib/blockMesh/blockMeshTopology.C
@@ -24,7 +24,7 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "blockMesh.H"
-#include "Time.H"
+#include "Time.T.H"
 #include "preservePatchTypes.H"
 #include "emptyPolyPatch.H"
 #include "cyclicPolyPatch.H"
